ddf26a60c740ac5de0370524b46cb8ad1f66dca5
yuxxeun <itsyuxxeun@gmail.com>
2022-11-22 17:15:06 +0700

    :fire: init

---
+++ README.md
@@ -0,0 +1,5 @@
+<p align="center"><a href="https://github.com/yuxxeun/jasmine" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400" alt="Laravel Logo"></a></p>
+
+## License
+
+Jasmine is open-sourced software licensed under the [MIT license](./license).
---
+++ app/Models/Chirp.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+use App\Events\ChirpCreated;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Chirp extends Model
+{
+    use HasFactory;
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    protected $fillable = [
+        'message',
+    ];
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
+}
